aa2f66d8cb34ad24e0a91af3fa485a3a1b2aea12
Андрей Сливка <ai.slivka@stc.team>
2023-12-17 22:27:03 +0300

    add vite-plugin-image-optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from "node:url"
 
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vitePluginImp from "vite-plugin-imp"
@@ -12,6 +13,7 @@ export default defineConfig({
   base: process.env.BASE_URL || "/",
   plugins: [
     vue(),
+    ViteImageOptimizer(),
     svgLoader({
       defaultImport: "component",
     }),
@@ -69,8 +71,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@/comp": fileURLToPath(new URL("./src/components", import.meta.url)),
-      "@/api": fileURLToPath(new URL("./src/api", import.meta.url)),
     },
   },
 })
